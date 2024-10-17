10aa31cc61a2ece4190c308533c8309119df6f3b
TatsuyaKashiwa <sachikaze417@gmail.com>
2024-10-17 19:43:54 +0900

    エンコード一覧とそれを用いてエンコード(Encodingクラスフィールド)の出力を行うEncodesクラスを作成

---
+++ TextEditor/Encodes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextEditor
+{
+    internal static class Encodes
+    {
+        static Encoding[] encodings = {Encoding.UTF8,Encoding.Unicode,Encoding.BigEndianUnicode,Encoding.UTF32 };
+        static Encoding ChangeEncode(int i) => encodings[i];
+    }
+}
