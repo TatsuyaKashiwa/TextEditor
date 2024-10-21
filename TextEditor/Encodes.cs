7d22f4a49f2359b0aca0ac2c09740f7c46b206d2
TatsuyaKashiwa <sachikaze417@gmail.com>
2024-10-21 21:38:02 +0900

    コメント一部入力・textChengedメソッド記述変更

--- TextEditor/Encodes.cs
+++ TextEditor/Encodes.cs
@@ -6,11 +6,25 @@ using System.Threading.Tasks;
 
 namespace TextEditor
 {
+    //エンコードに関することはForm1クラス内の様々な所に現れている
+    //なので、エンコード関連の処理をひとまとめにしたほうが効率が良く読みやすくなると思われる
+    //エンコード関連のことをまとめたユーティリティクラスを作成した
     internal static class Encodes
     {
+        //エンコードを変化・選択させるのにインデックス・int32型の値として扱えれば便利である
+        //ただし、選択するエンコードの種類はアプリケーション利用中に追加することはないため
+        //(通常の)配列としてアプリケーションで用いるエンコードを格納した
+        //(下記メソッドを介して取り扱われるためprivateとした)
         static Encoding[] encodings = {Encoding.UTF8,Encoding.Unicode,Encoding.BigEndianUnicode,Encoding.UTF32 };
+        
+        //上記の配列からエンコードを取り出す仕組みが必要となる
+        //外部のコントロールの選択と上記の配列を対応させる必要があるため
+        //引数にコントロールの選択に対応する値を取るメソッドとして定義した
         internal static Encoding GetEncode(int i) => encodings[i];
 
+        //エンコードを変化させただけでは文字化けとなり、ファイルの内容を保持したエンコード変換ができない。
+        //内容を保持したままエンコードを変換するには一旦バイト配列に変換する必要がある
+        //バイト配列への変換→エンコード変換→String型に変換して返す操作を一元化したメソッドを作成
         internal static string ChangeEncode(Encoding previousEncode, Encoding afterwardEncode, string text) 
         {
             byte[] previousByte = previousEncode.GetBytes(text);
--- TextEditor/Form1.cs
+++ TextEditor/Form1.cs
@@ -16,9 +16,16 @@ namespace TextEditor
         static Encoding encodeLoad = Encodes.GetEncode(0);
         static Encoding encodeSave = Encodes.GetEncode(0);
         static Color tagColor = Color.Blue;
-       
+
+        //xmlファイルのタグに色を付けるためにxmlファイルであるかを判定する必要がある
+        //判定結果を boolとしたほうが、条件式にそのまま組み込めるので
+        //引数のファイルパスが.xmlで終わるかの判定をIsMatchメソッドで実現した
         bool isXML(string path) => Regex.IsMatch(path, ".xml$");
 
+        //タグの要素のみを別の色へ変更する必要がある
+        //タグに合致する表現を見つけ出して、要素だけを色付けする必要があるため
+        //タグの条件に合致する表現を配列に格納し
+        //要素部分だけをタグ色で指定した色へ変更させた。
         void ColoringTag()
         {
             var tags = Regex.Matches(richTextBox.Text, @"<([^<>]+)>");
@@ -31,6 +38,9 @@ namespace TextEditor
             }
         }
 
+        //入力・出力共にファイルパスを得ることは共通であるのでメソッドとして共通化すべきである
+        //入出力先のパスは別になり得るため、得られたファイルパスを返り値(string型)として返す必要がある
+        //パスを得るためのif文だけでなく、else文としてファイル選択されなかった際の返り値を設定した形とした
         string GetFilepath()
         {
             if (openFileDialog.ShowDialog() == DialogResult.OK)
@@ -43,6 +53,7 @@ namespace TextEditor
             }
         }
 
+        
         private void load_Click(object sender, EventArgs e)
         {
             loadedfilepath = GetFilepath();
@@ -85,6 +96,9 @@ namespace TextEditor
             }
         }
 
+        //ラジオボタンの選択で読み込み/保存のエンコードを変えられるようにしたい
+        //ラジオボタンの選択をエンコードを返すメソッドに渡す必要があるため
+        //各ボタンのエンコードに対応するint型の値を返すようにした
         private void utf8_CheckedChanged(object sender, EventArgs e) => encodeNum = 0;
 
         private void utf16le_CheckedChanged(object sender, EventArgs e) => encodeNum = 1;
@@ -93,6 +107,9 @@ namespace TextEditor
 
         private void utf32_CheckedChanged(object sender, EventArgs e) => encodeNum = 3;
 
+        //タグ要素の色を変更せずにテキストの色を変更したい
+        //なぜなら、タグ要素の色付けが一時的なものとなってしまうため
+        //テキスト色変更後にタグの色を付けなおした
         private void changeTextColor_Click(object sender, EventArgs e)
         {
             if (colorDialogText.ShowDialog() == DialogResult.OK)
@@ -102,6 +119,9 @@ namespace TextEditor
             }
         }
 
+        //タグ要素の色を変更したい
+        //ただプロパティを変更するだけでは色は変わらないため
+        //要素の色を決定した後タグ色を変更させるメソッドを作用させた
         private void changeTagColor_Click(object sender, EventArgs e)
         {
             if (isXML(loadedfilepath) && (colorDialogTag.ShowDialog() == DialogResult.OK)) 
@@ -111,20 +131,16 @@ namespace TextEditor
             }
         }
 
+        //テキスト入力時にタグの形式になっていれば自動で色がついてほしい
+        //タグ要素の色でテキストが入力されてはならない、かつ入力場所も変化してはならないため
+        //通常のタグ要素色変更メソッドに加えて入力位置を元に戻し、タグの選択を解除する記述を追加した
         private void textChanged(object sender, EventArgs e)
         {
             if (isXML(loadedfilepath))
             {
                 int currentPosition = richTextBox.SelectionStart;
                 string alltext = richTextBox.Text;
-                var tags = Regex.Matches(richTextBox.Text, @"<([^<>]+)>");
-                foreach (Match tag in tags)
-                {
-                    var index = tag.Groups[1].Index;
-                    var tagLength = tag.Groups[1].Length;
-                    richTextBox.Select(index, tagLength);
-                    richTextBox.SelectionColor = tagColor;
-                }
+                ColoringTag();
                 richTextBox.SelectionStart = currentPosition;
                 richTextBox.SelectionLength = 0;
             }
