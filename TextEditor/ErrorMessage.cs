4643cb87e3388e7ea5ae3286b6909b4a3525372b
TatsuyaKashiwa <sachikaze417@gmail.com>
2024-10-26 15:56:35 +0900

    ErrorMessageクラスのコメントをXMLコメント化

--- TextEditor/ErrorMessage.cs
+++ TextEditor/ErrorMessage.cs
@@ -6,15 +6,25 @@ using System.Threading.Tasks;
 
 namespace TextEditor
 {
-    //Form1クラスが乱雑になるのを防ぐべく
-    //例外に対する処理を分離して記述するため
-    //ErrorMessageクラスを作成した。
+    /// <summary>
+    /// エラーメッセージ表示のためのクラス
+    /// </summary>
+    /// <remarks>
+    /// Form1クラスが乱雑になるのを防ぐべく
+    ///例外に対する処理を分離して記述するためのクラス
+    ///</remarks>
     internal static class ErrorMessage
     {
 
-        //例外に対する扱いはその種類に関わらずメッセージボックスを出すことで統一
-        //対応すべき例外は2種類から増える可能性がある
-        //各例外に対応するメッセージを、例外の数が増えた時の拡張性が高いswitch文による記述で分岐させて表示
+        /// <summary>
+        /// catchされた例外に対応するエラーメッセージを表示させるメソッド
+        /// </summary>
+        /// <param name="e">catchされた例外</param>
+        /// <remarks>
+        /// 例外に対する扱いはその種類に関わらずメッセージボックスを出すことで統一
+        ///対応すべき例外は2種類から増える可能性がある
+        ///各例外に対応するメッセージを、例外の数が増えた時の拡張性が高いswitch文による記述で分岐させて表示
+        ///</remarks>
         internal static void ShowErrorMessage(Exception e) 
         {
             switch (e) 
