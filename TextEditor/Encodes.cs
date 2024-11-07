7b87caca6bf33006ef15bfc744f3a826ad2ebfa5
user\TatsuyaKashiwa <TKashi0@outlook.jp>
2024-11-07 15:55:09 +0900

    名前空間をファイルスコープ名前空間へ

--- TextEditor/Encodes.cs
+++ TextEditor/Encodes.cs
@@ -4,60 +4,59 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TextEditor
+namespace TextEditor;
+
+/// <summary>
+/// エンコードに関わるクラス
+/// </summary>
+/// <remarks>
+/// エンコードに関することはForm1クラス内の様々な所に現れている
+///なので、エンコード関連の処理をひとまとめにしたほうが効率が良く読みやすくなると思われる
+///エンコード関連のことをまとめたユーティリティクラスを作成した
+///</remarks>
+
+internal static class Encodes
 {
     /// <summary>
-    /// エンコードに関わるクラス
+    /// 利用エンコードをまとめた配列
     /// </summary>
+    ///  <remarks>
+    ///エンコードを変化・選択させるのにインデックス・int32型の値として扱えれば便利である
+    ///ただし、選択するエンコードの種類はアプリケーション利用中に追加することはないため
+    ///(通常の)配列としてアプリケーションで用いるエンコードを格納した
+    ///(下記メソッドを介して取り扱われるためprivateとした)
+    /// </remarks>
+    private static Encoding[] _encodings = {Encoding.UTF8,Encoding.Unicode,Encoding.BigEndianUnicode,Encoding.UTF32 };
+
+    /// <summary>
+    /// 上記の配列からエンコードを取り出す仕組みが必要となる
+    /// </summary>
+    /// <param name="i">エンコードに対応するインデックス</param>
+    /// <returns>引数(ラジオボタンの選択)に対応したエンコード</returns>
     /// <remarks>
-    /// エンコードに関することはForm1クラス内の様々な所に現れている
-    ///なので、エンコード関連の処理をひとまとめにしたほうが効率が良く読みやすくなると思われる
-    ///エンコード関連のことをまとめたユーティリティクラスを作成した
+    ///外部のコントロールの選択と上記の配列を対応させる必要があるため
+    ///引数にコントロールの選択に対応する値を取るメソッドとして定義した
     ///</remarks>
+    internal static Encoding GetEncode(int i) => _encodings[i];
 
-    internal static class Encodes
+    /// <summary>
+    /// 入力文字のバイト配列を選択したエンコードに変更し、入力文字のエンコードとバイト配列を一致させる
+    /// </summary>
+    /// <param name="previousEncode">現在のエンコード</param>
+    /// <param name="afterwardEncode">変換先のエンコード</param>
+    /// <param name="text">入力テキスト（エンコード変換前）</param>
+    /// <returns>入力テキスト(エンコード変換後)</returns>
+    /// <remarks>
+    /// エンコードを変化させただけでは文字化けとなり、ファイルの内容を保持したエンコード変換ができない。
+    ///内容を保持したままエンコードを変換するには一旦バイト配列に変換する必要がある
+    ///バイト配列への変換→エンコード変換→String型に変換して返す操作を一元化したメソッドを作成
+    ///</remarks>
+    internal static string ChangeEncode(Encoding previousEncode, Encoding afterwardEncode, string text) 
     {
-        /// <summary>
-        /// 利用エンコードをまとめた配列
-        /// </summary>
-        ///  <remarks>
-        ///エンコードを変化・選択させるのにインデックス・int32型の値として扱えれば便利である
-        ///ただし、選択するエンコードの種類はアプリケーション利用中に追加することはないため
-        ///(通常の)配列としてアプリケーションで用いるエンコードを格納した
-        ///(下記メソッドを介して取り扱われるためprivateとした)
-        /// </remarks>
-        private static Encoding[] _encodings = {Encoding.UTF8,Encoding.Unicode,Encoding.BigEndianUnicode,Encoding.UTF32 };
-
-        /// <summary>
-        /// 上記の配列からエンコードを取り出す仕組みが必要となる
-        /// </summary>
-        /// <param name="i">エンコードに対応するインデックス</param>
-        /// <returns>引数(ラジオボタンの選択)に対応したエンコード</returns>
-        /// <remarks>
-        ///外部のコントロールの選択と上記の配列を対応させる必要があるため
-        ///引数にコントロールの選択に対応する値を取るメソッドとして定義した
-        ///</remarks>
-        internal static Encoding GetEncode(int i) => _encodings[i];
-
-        /// <summary>
-        /// 入力文字のバイト配列を選択したエンコードに変更し、入力文字のエンコードとバイト配列を一致させる
-        /// </summary>
-        /// <param name="previousEncode">現在のエンコード</param>
-        /// <param name="afterwardEncode">変換先のエンコード</param>
-        /// <param name="text">入力テキスト（エンコード変換前）</param>
-        /// <returns>入力テキスト(エンコード変換後)</returns>
-        /// <remarks>
-        /// エンコードを変化させただけでは文字化けとなり、ファイルの内容を保持したエンコード変換ができない。
-        ///内容を保持したままエンコードを変換するには一旦バイト配列に変換する必要がある
-        ///バイト配列への変換→エンコード変換→String型に変換して返す操作を一元化したメソッドを作成
-        ///</remarks>
-        internal static string ChangeEncode(Encoding previousEncode, Encoding afterwardEncode, string text) 
-        {
-            byte[] previousByte = previousEncode.GetBytes(text);
-            byte[] afterwardByte = Encoding.Convert(previousEncode, afterwardEncode, previousByte);
-            return afterwardEncode.GetString(afterwardByte);
-        }
-        
-        
+        byte[] previousByte = previousEncode.GetBytes(text);
+        byte[] afterwardByte = Encoding.Convert(previousEncode, afterwardEncode, previousByte);
+        return afterwardEncode.GetString(afterwardByte);
     }
+    
+    
 }
--- TextEditor/MainForm.cs
+++ TextEditor/MainForm.cs
@@ -1,186 +1,224 @@
 using System.Text;
 using System.Text.RegularExpressions;
 
-namespace TextEditor
+namespace TextEditor;
+
+public partial class MainForm : Form
 {
-    public partial class MainForm : Form
+    public MainForm()
     {
-        public MainForm()
+        this.InitializeComponent();
+    }
+
+    private string _loadedFilePath = "";
+    private string _savingFilePath = "";
+    private int _encodeNum = 0;
+    private Encoding _encodeLoad = Encodes.GetEncode(0);
+    private Encoding _encodeSave = Encodes.GetEncode(0);
+    private Color _tagColor = Color.Blue;
+
+    /// <summary>
+    /// XMLファイルであるかを判定するメソッド
+    /// </summary>
+    /// <param name="path">判定対象のファイルパス</param>
+    /// <returns>XMLファイルであればtrueを返す</returns>
+    /// <remarks>
+    ///xmlファイルのタグに色を付けるためにxmlファイルであるかを判定する必要がある
+    ///判定結果を boolとしたほうが、条件式にそのまま組み込めるので
+    ///引数のファイルパスが.xmlで終わるかの判定をIsMatchメソッドで実現した
+    /// </remarks>
+    private bool IsXML(string path) => Regex.IsMatch(path, ".xml$");
+
+    /// <summary>
+    /// (XML)タグ要素の色を変更
+    /// </summary>
+    ///<remarks>
+    ///タグの要素のみを別の色へ変更する必要がある
+    ///タグに合致する表現を見つけ出して、要素だけを色付けする必要があるため
+    ///タグの条件に合致する表現を配列に格納し
+    ///要素部分だけをタグ色で指定した色へ変更させた。
+    ///</remarks>
+    private void ColoringTag()
+    {
+        var tags = Regex.Matches(this.RichTextBox.Text, @"<([^<>]+)>");
+        foreach (Match tag in tags)
         {
-            this.InitializeComponent();
+            var index = tag.Groups[1].Index;
+            var tagLength = tag.Groups[1].Length;
+            this.RichTextBox.Select(index, tagLength);
+            this.RichTextBox.SelectionColor = this._tagColor;
         }
+    }
 
-        private string _loadedFilePath = "";
-        private string _savingFilePath = "";
-        private int _encodeNum = 0;
-        private Encoding _encodeLoad = Encodes.GetEncode(0);
-        private Encoding _encodeSave = Encodes.GetEncode(0);
-        private Color _tagColor = Color.Blue;
-
-        /// <summary>
-        /// XMLファイルであるかを判定するメソッド
-        /// </summary>
-        /// <param name="path">判定対象のファイルパス</param>
-        /// <returns>XMLファイルであればtrueを返す</returns>
-        /// <remarks>
-        ///xmlファイルのタグに色を付けるためにxmlファイルであるかを判定する必要がある
-        ///判定結果を boolとしたほうが、条件式にそのまま組み込めるので
-        ///引数のファイルパスが.xmlで終わるかの判定をIsMatchメソッドで実現した
-        /// </remarks>
-        private bool IsXML(string path) => Regex.IsMatch(path, ".xml$");
-
-        /// <summary>
-        /// (XML)タグ要素の色を変更
-        /// </summary>
-        ///<remarks>
-        ///タグの要素のみを別の色へ変更する必要がある
-        ///タグに合致する表現を見つけ出して、要素だけを色付けする必要があるため
-        ///タグの条件に合致する表現を配列に格納し
-        ///要素部分だけをタグ色で指定した色へ変更させた。
-        ///</remarks>
-        private void ColoringTag()
+    /// <summary>
+    /// ファイル読み込み(loadボタン押下時の動作)に対応するメソッド
+    /// </summary>
+    /// <param name="ex">catchした例外</param>
+    /// <remarks>
+    /// ファイルを選択したエンコードで取り込み、XMLファイルであれば色を付けたい
+    ///すべて取り込むReadAllLinesメソッドでは動作が重くなりうるため(ファイルサイズが莫大ならば→現状はインスタンス生成コストのほうが重いのでAllLinesで)
+    ///テキストの読み込みとタグの色付けは一連の動作としたいため
+    ///一行ずつ表示領域に追加した後、ファイル取り込み以降の操作をtry節で囲んだ
+    ///</remarks>
+    ///<exception cref="System.ArgumentException">ファイル未選択の場合</exception>
+    ///<exception cref="System.IO.FileNotFoundException">存在しないファイル名を入力した場合</exception>
+    private void Load_Click(object sender, EventArgs e)
+    {
+        if (this.OpenFileDialog.ShowDialog() == DialogResult.OK)
         {
-            var tags = Regex.Matches(this.RichTextBox.Text, @"<([^<>]+)>");
-            foreach (Match tag in tags)
+            this._loadedFilePath = this.OpenFileDialog.FileName;
+        }
+        this._encodeLoad = Encodes.GetEncode(this._encodeNum);
+        try
+        {
+            this.RichTextBox.Text = this.MyReadAllText(this._loadedFilePath, this._encodeLoad);
+            if (this.IsXML(this._loadedFilePath))
             {
-                var index = tag.Groups[1].Index;
-                var tagLength = tag.Groups[1].Length;
-                this.RichTextBox.Select(index, tagLength);
+                this.ColoringTag();
                 this.RichTextBox.SelectionColor = this._tagColor;
             }
         }
-
-        /// <summary>
-        /// ファイル読み込み(loadボタン押下時の動作)に対応するメソッド
-        /// </summary>
-        /// <param name="ex">catchした例外</param>
-        /// <remarks>
-        /// ファイルを選択したエンコードで取り込み、XMLファイルであれば色を付けたい
-        ///すべて取り込むReadAllLinesメソッドでは動作が重くなりうるため(ファイルサイズが莫大ならば→現状はインスタンス生成コストのほうが重いのでAllLinesで)
-        ///テキストの読み込みとタグの色付けは一連の動作としたいため
-        ///一行ずつ表示領域に追加した後、ファイル取り込み以降の操作をtry節で囲んだ
-        ///</remarks>
-        ///<exception cref="System.ArgumentException">ファイル未選択の場合</exception>
-        ///<exception cref="System.IO.FileNotFoundException">存在しないファイル名を入力した場合</exception>
-        private void Load_Click(object sender, EventArgs e)
+        catch (Exception ex)
         {
-            if (this.OpenFileDialog.ShowDialog() == DialogResult.OK)
-            {
-                this._loadedFilePath = this.OpenFileDialog.FileName;
-            }
-            this._encodeLoad = Encodes.GetEncode(this._encodeNum);
-            try
-            {
-                this.RichTextBox.Text = File.ReadAllText(this._loadedFilePath, this._encodeLoad);
-                if (this.IsXML(this._loadedFilePath))
-                {
-                    this.ColoringTag();
-                    this.RichTextBox.SelectionColor = this._tagColor;
-                }
-            }
-            catch (Exception ex)
-            {
-                ErrorMessage.ShowErrorMessage(ex);
-            }
+            ErrorMessage.ShowErrorMessage(ex);
         }
+    }
 
-        /// <summary>
-        /// ファイル保存(saveボタン押下)に対応するメソッド
-        /// </summary>
-        /// <param name="ex">catchした例外</param>
-        /// <remarks>
-        /// ファイルを指定したエンコードで保存、ファイルを新たに作り出すことも行いたい
-        ///ファイルのエンコードはファイルを適切なエンコードとして保存するために必要であるので
-        ///Encodesクラスのメソッドでエンコードを変換し、
-        ///WriteAllTextはファイルが存在しない場合は作成するため、この部分をtry節で囲んだ
-        ///</remarks>
-        ///<exception cref="System.ArgumentException">ファイル未選択の場合</exception>
-        private void Save_Click(object sender, EventArgs e)
+    /// <summary>
+    /// ファイル保存(saveボタン押下)に対応するメソッド
+    /// </summary>
+    /// <param name="ex">catchした例外</param>
+    /// <remarks>
+    /// ファイルを指定したエンコードで保存、ファイルを新たに作り出すことも行いたい
+    ///ファイルのエンコードはファイルを適切なエンコードとして保存するために必要であるので
+    ///Encodesクラスのメソッドでエンコードを変換し、
+    ///WriteAllTextはファイルが存在しない場合は作成するため、この部分をtry節で囲んだ
+    ///</remarks>
+    ///<exception cref="System.ArgumentException">ファイル未選択の場合</exception>
+    private void Save_Click(object sender, EventArgs e)
+    {
+        this._encodeSave = Encodes.GetEncode(this._encodeNum);
+        this.RichTextBox.Text = Encodes.ChangeEncode(this._encodeLoad, this._encodeSave, this.RichTextBox.Text);
+        if (this.SaveFileDialog.ShowDialog() == DialogResult.OK)
         {
-            this._encodeSave = Encodes.GetEncode(this._encodeNum);
-            this.RichTextBox.Text = Encodes.ChangeEncode(this._encodeLoad, this._encodeSave, this.RichTextBox.Text);
-            if (this.SaveFileDialog.ShowDialog() == DialogResult.OK)
-            {
-                this._savingFilePath = this.SaveFileDialog.FileName;
-            }
-            try
-            {
-                    File.WriteAllText(this._savingFilePath, this.RichTextBox.Text, this._encodeSave);
-            }
-            catch (Exception ex)
-            {
-                ErrorMessage.ShowErrorMessage(ex);
-            }
+            this._savingFilePath = this.SaveFileDialog.FileName;
         }
-
-        /// <summary>
-        /// ラジオボタンに対応するエンコード（に対応するインデックス）を選択するメソッド
-        /// </summary>
-        /// <remarks>
-        /// ラジオボタンの選択で読み込み/保存のエンコードを変えられるようにしたい
-        ///ラジオボタンの選択をエンコードを返すメソッドに渡す必要があるため
-        ///各ボタンのエンコードに対応するint型の値を返すようにした
-        ///</remarks>
-        private void Utf8_CheckedChanged(object sender, EventArgs e) => this._encodeNum = 0;
-
-        private void Utf16le_CheckedChanged(object sender, EventArgs e) => this._encodeNum = 1;
-
-        private void Utf16be_CheckedChanged(object sender, EventArgs e) => this._encodeNum = 2;
-
-        private void Utf32_CheckedChanged(object sender, EventArgs e) => this._encodeNum = 3;
-
-        /// <summary>
-        /// テキスト色変更のメソッド
-        /// </summary>
-        /// <remarks>
-        /// タグ要素の色を変更せずにテキストの色を変更したい
-        ///なぜなら、タグ要素の色付けが一時的なものとなってしまうため
-        ///テキスト色変更後にタグの色を付けなおした
-        ///</remarks>
-        private void ChangeTextColor_Click(object sender, EventArgs e)
+        try
         {
-            if (this.ColorDialogText.ShowDialog() == DialogResult.OK)
-            {
-                this.RichTextBox.ForeColor = this.ColorDialogText.Color;
-                this.ColoringTag();
-            }
+                this.MyWriteAllText(this._savingFilePath, this.RichTextBox.Text, this._encodeSave);
         }
-
-        /// <summary>
-        /// タグ要素の色を変更するメソッド
-        /// </summary>
-        /// <remarks>
-        ///  タグ要素の色を変更したい
-        ///ただプロパティを変更するだけでは色は変わらないため
-        ///要素の色を決定した後タグ色を変更させるメソッドを作用させた
-        ///</remarks>
-        private void ChangeTagColor_Click(object sender, EventArgs e)
+        catch (Exception ex)
         {
-            if (this.IsXML(this._loadedFilePath) && (this.ColorDialogTag.ShowDialog() == DialogResult.OK))
-            {
-                this._tagColor = this.ColorDialogTag.Color;
-                this.ColoringTag();
-            }
+            ErrorMessage.ShowErrorMessage(ex);
         }
+    }
 
-        /// <summary>
-        /// テキスト入力時にタグの形式になっていればタグ要素を着色するメソッド
-        /// </summary>
-        /// <remarks>
-        /// テキスト入力時にタグの形式になっていれば自動で色がついてほしい
-        ///タグ要素の色でテキストが入力されてはならない、かつ入力場所も変化してはならないため
-        ///通常のタグ要素色変更メソッドに加えて入力位置を元に戻し、タグの選択を解除する記述を追加した
-        ///</remarks>
-        private void TextChanging(object sender, EventArgs e)
-       {
-            if (this.IsXML(this._loadedFilePath))
-            {
-                int currentPosition = this.RichTextBox.SelectionStart;
-                this.ColoringTag();
-                this.RichTextBox.SelectionStart = currentPosition;
-                this.RichTextBox.SelectionLength = 0;
-            }
+    /// <summary>
+    /// ラジオボタンに対応するエンコード（に対応するインデックス）を選択するメソッド
+    /// </summary>
+    /// <remarks>
+    /// ラジオボタンの選択で読み込み/保存のエンコードを変えられるようにしたい
+    ///ラジオボタンの選択をエンコードを返すメソッドに渡す必要があるため
+    ///各ボタンのエンコードに対応するint型の値を返すようにした
+    ///</remarks>
+    private void Utf8_CheckedChanged(object sender, EventArgs e) => this._encodeNum = 0;
+
+    private void Utf16le_CheckedChanged(object sender, EventArgs e) => this._encodeNum = 1;
+
+    private void Utf16be_CheckedChanged(object sender, EventArgs e) => this._encodeNum = 2;
+
+    private void Utf32_CheckedChanged(object sender, EventArgs e) => this._encodeNum = 3;
+
+    /// <summary>
+    /// テキスト色変更のメソッド
+    /// </summary>
+    /// <remarks>
+    /// タグ要素の色を変更せずにテキストの色を変更したい
+    ///なぜなら、タグ要素の色付けが一時的なものとなってしまうため
+    ///テキスト色変更後にタグの色を付けなおした
+    ///</remarks>
+    private void ChangeTextColor_Click(object sender, EventArgs e)
+    {
+        if (this.ColorDialogText.ShowDialog() == DialogResult.OK)
+        {
+            this.RichTextBox.ForeColor = this.ColorDialogText.Color;
+            this.ColoringTag();
+        }
+    }
+
+    /// <summary>
+    /// タグ要素の色を変更するメソッド
+    /// </summary>
+    /// <remarks>
+    ///  タグ要素の色を変更したい
+    ///ただプロパティを変更するだけでは色は変わらないため
+    ///要素の色を決定した後タグ色を変更させるメソッドを作用させた
+    ///</remarks>
+    private void ChangeTagColor_Click(object sender, EventArgs e)
+    {
+        if (this.IsXML(this._loadedFilePath) && (this.ColorDialogTag.ShowDialog() == DialogResult.OK))
+        {
+            this._tagColor = this.ColorDialogTag.Color;
+            this.ColoringTag();
+        }
+    }
+
+    /// <summary>
+    /// テキスト入力時にタグの形式になっていればタグ要素を着色するメソッド
+    /// </summary>
+    /// <remarks>
+    /// テキスト入力時にタグの形式になっていれば自動で色がついてほしい
+    ///タグ要素の色でテキストが入力されてはならない、かつ入力場所も変化してはならないため
+    ///通常のタグ要素色変更メソッドに加えて入力位置を元に戻し、タグの選択を解除する記述を追加した
+    ///</remarks>
+    private void TextChanging(object sender, EventArgs e)
+   {
+        if (this.IsXML(this._loadedFilePath))
+        {
+            int currentPosition = this.RichTextBox.SelectionStart;
+            this.ColoringTag();
+            this.RichTextBox.SelectionStart = currentPosition;
+            this.RichTextBox.SelectionLength = 0;
+        }
+    }
+
+
+    /// <summary>
+    /// ReadAllTextの自作版メソッド
+    /// </summary>
+    /// <param name="path">読み込みファイルのフルパス</param>
+    /// <param name="encode">読み込みエンコード</param>
+    /// <returns>ファイル内容(string型)</returns>
+    /// <remarks>
+    /// ReadAllTextをStreamを明記して作成
+    /// StreamReaderをnewすることでFileStreamをOpenし、
+    /// StreamReaderのReadToEnd()メソッドで最終行まで読み込み
+    /// </remarks>
+    private string MyReadAllText(string path, Encoding encode) 
+    {
+        using (var sreader = new StreamReader(path,encode)) 
+        {
+            return sreader.ReadToEnd();
         }
 
     }
+
+    /// <summary>
+    /// WriteAllTextの自作版メソッド
+    /// </summary>
+    /// <param name="path">保存ファイルパス</param>
+    /// <param name="savetext">保存するテキスト</param>
+    /// <param name="encode">エンコード</param>
+    /// <remarks>
+    /// StreamWriterをnewすることで(ファイル作成・)FileStreamをOpenして
+    /// 引数として受けた保存テキストを、StreamWriter.Writeでファイルへ書き込み
+    /// </remarks>
+    private void MyWriteAllText(string path, string savetext, Encoding encode) 
+    {
+        using (var sw = new StreamWriter(path,false,encode)) 
+        {
+            sw.Write(savetext);
+        }
+    }
+
 }
